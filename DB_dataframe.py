363c0683157782295258b30526aed7ee6fa69aea
simhyeji <simhyeji70@gmail.com>
2022-11-30 10:30:41 +0900

    DB데이터프레임화 수정

--- DB_dataframe.py
+++ DB_dataframe.py
@@ -1,9 +1,8 @@
 import pymysql
 import pandas as pd
-# to_do: 불러올 길이, 연결할 데이터 로그인 인자 받아오기 등 : 클래스화하기
 
-class DB_data():
-    def __init__(self, host:str, user:str, password:str, db:str, data_limit:int ):    #(호스트명, 유저아이디, 비밀번호, db명, 불어올 데이터 양) 설정
+class DB_dataframe():
+    def __init__(self, host:str, user:str, password:str, db:str, data_limit:int ):    
         self.host = host
         self.user = user
         self.password = password
@@ -14,28 +13,29 @@ class DB_data():
         data1, data2, data3, data4 = "", "", "", ""
         row=None
 
+        self.DB_connect()
+
     #DB 연결
     def DB_connect(self):
-        #conn = pymysql.connect(host='localhost', user='root', password='1111', db='shopDB', charset='utf8')
+        #conn = pymysql.connect(host='localhost', user='root', password='1111', db='shopDB', charset='utf8') 
         conn = pymysql.connect(host=self.host, user=self.user, password=self.password, db=self.db, charset='utf8')
         cur = conn.cursor()
 
-        DB_data.DB_dataload(cur, self.data_limit)
-        #result = print(DB_data.DB_dataload(cur, self.data_limit)) #return 값으로 받아 변수에 df 저장하는 방식
+        self.DB_dataload(cur)
+        #result = print(DB_dataframe.DB_dataload(cur, self.data_limit)) #return 값으로 받아 변수에 df 저장하는 방식
 
         conn.close()
 
 
 
     #DB 데이터 가져오기 
-    def DB_dataload(cur, data_limit):
-        sql1 = "SELECT * FROM userTable"
-        sql2 = "select id, email from userTable"  #sql부에 조건 변경해서 가져올 데이터 설정(데이터 컬럼명, where 등 변경해야함)
-        cur.execute(sql2)
+    def DB_dataload(self, cur):
+        sql = "select id, email from userTable"  #sql부에 조건 변경해서 가져올 데이터 설정(데이터 컬럼명, where 등 변경해야함)
+        cur.execute(sql)
 
         data_list = []
         data_len = 0
-        while (data_len < data_limit) :
+        while (data_len < self.data_limit) :
             # 루프를 돌면서 tuple 하나씩 가져오기
             row = cur.fetchone()
             #print(row)
@@ -50,12 +50,14 @@ class DB_data():
             data_len += 1
         #print(data_list)
 
-        #데이터 프레임화 추가 (데이터 프레임화 필요없으면 리스트화도 그냥 제거하기)
+        #데이터 프레임화
         df = pd.DataFrame(data_list)
         df.columns = ['ID', 'Email']
         print(df)
         return df
 
-DB_exc = DB_data('localhost', 'root', '1111', 'shopDB', 2)
-DB_exc.DB_connect()
+
+#함수 실행방법, DB_dataframe(호스트명, 유저아이디, 비밀번호, db명, 불어올 데이터 양 지정) 
+#DB_dataframe('localhost', 'root', '1111', 'shopDB', 2)
+
 
